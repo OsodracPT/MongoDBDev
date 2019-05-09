29003d42c87d09b9d6094561e46bf654d0964029
Pedro Cardoso <pedroosodracwork@gmail.com>
2019-05-09 23:42:17 +0100

    Sorting classes

--- MongoDBDemo/Program.cs
+++ MongoDBDemo/Program.cs
@@ -1,8 +1,4 @@
-﻿using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
-using MongoDB.Driver;
-using System;
-using System.Collections.Generic;
+﻿using System;
 
 namespace MongoDBDemo
 {
@@ -26,93 +22,20 @@ namespace MongoDBDemo
             //};
             //db.InsertRecord("Users", person);
 
-            //var recs = db.LoadRecords<PersonModel>("Users");
+            var recs = db.LoadRecords<NameModel>("Users");
 
-            //foreach (var rec in recs)
-            //{
-            //    Console.WriteLine($"{rec.Id}: {rec.FirstName} {rec.LastName}");
+            foreach (var rec in recs)
+            {
+                Console.WriteLine($"{rec.FirstName} {rec.LastName}");
+                Console.WriteLine();
+            }
 
-            //    if(rec.PrimaryAddress!= null)
-            //    {
-            //        Console.WriteLine(rec.PrimaryAddress.City);
-            //    }
-            //    Console.WriteLine();
-            //}
-
-            var oneRec = db.LoadRecordById<PersonModel>("Users", new Guid("602879cf-d2db-4f1d-9db6-845817322e38"));
+            //var oneRec = db.LoadRecordById<PersonModel>("Users", new Guid("602879cf-d2db-4f1d-9db6-845817322e38"));
             //oneRec.DateOfBirth = new DateTime(1982, 10, 31, 0, 0, 0, DateTimeKind.Utc);
             //db.UpsertRecord("Users", oneRec.Id, oneRec);
 
-            db.DeleteRecord<PersonModel>("Users", oneRec.Id);
+            //db.DeleteRecord<PersonModel>("Users", oneRec.Id);
             Console.ReadLine();
-        }
-    }
-
-    public class PersonModel
-    {
-        [BsonId]
-        public Guid Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public AddressModel PrimaryAddress { get; set; }
-        [BsonElement("dob")]
-        public DateTime DateOfBirth { get; set; }
-    }
-
-    public class AddressModel
-    {
-        public string StreetAddress { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public string ZipCode { get; set; }
-    }
-
-    public class MongoCRUD
-    {
-        private IMongoDatabase db;
-        public MongoCRUD(string database)
-        {
-            var client = new MongoClient();
-            db = client.GetDatabase(database);
-        }
-
-        public void InsertRecord<T>(string table, T record)
-        {
-            var collection = db.GetCollection<T>(table);
-            collection.InsertOne(record);
-        }
-
-        public List<T> LoadRecords<T>(string table)
-        {
-            var collection = db.GetCollection<T>(table);
-
-            return collection.Find(new BsonDocument()).ToList();
-        }
-
-        public T LoadRecordById<T>(string table, Guid id)
-        {
-            var collection = db.GetCollection<T>(table);
-            var filter = Builders<T>.Filter.Eq("Id", id);
-
-            return collection.Find(filter).First();
-        }
-
-        public void UpsertRecord<T>(string table, Guid id, T record)
-        {
-            var collection = db.GetCollection<T>(table);
-
-            var result = collection.ReplaceOne(new BsonDocument("_id", id),
-                record,
-                new UpdateOptions { IsUpsert = true }); 
-        }
-
-        public void DeleteRecord<T>(string table, Guid id)
-        {
-            var collection = db.GetCollection<T>(table);
-            var filter = Builders<T>.Filter.Eq("Id", id);
-
-            collection.DeleteOne(filter);
-
         }
     }
 }
