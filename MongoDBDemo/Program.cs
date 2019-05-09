e0a0ac9e777d3bd4183a2a73332f669b791068b6
Pedro Cardoso <pedroosodracwork@gmail.com>
2019-05-09 22:26:19 +0100

    Insert Record

---
+++ MongoDBDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Driver;
+using System;
+
+namespace MongoDBDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoCRUD db = new MongoCRUD("AddressBook");
+            db.InsertRecord("Users", new PersonModel { FirstName = "Pedro", LasName="Cardoso"});
+            Console.ReadLine();
+        }
+    }
+
+    public class PersonModel
+    {
+       public string FirstName { get; set; }
+        public string LasName { get; set; }
+    }
+
+    public class MongoCRUD
+    {
+        private IMongoDatabase db;
+        public MongoCRUD(string database)
+        {
+            var client = new MongoClient();
+            db = client.GetDatabase(database);
+        }
+
+        public void InsertRecord<T>(string table, T record)
+        {
+            var collection = db.GetCollection<T>(table);
+            collection.InsertOne(record);
+        }
+    }
+}
